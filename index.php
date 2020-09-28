6139bee0e2eb894908d8fcbe20e87e7e3f292e79
StacyChebet <staceychebet23@gmail.com>
2020-09-28 12:48:38 +0300

    Added navbar with logo and links

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LOGIN</title>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+        <link href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Monoton&display=swap" rel="stylesheet">        <meta charset="utf-8">
+
+        <script src="js/index.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div id="topNavBar">
+                <img class="logo" src="images/camLogo.png">
+                <ul>
+                    <li id="title"><a href="index.php">BLACKGRAM</a></li>
+                    <li class="listItem"><a href="">Contact Us</a></li>
+                    <li class="listItem"><a href="">About</a></li>
+                    </ul>
+            </div>
+        </div> 
+        <!-- <div class="container2">
+            <div class="container3">
+                <h2 class="action"><br/>Order food from the comfort<br/> of your home</h2>
+                <p class="actionP">Click 'order' to order food from most of the restaurants near you!
+                </p>
+                <a href="userLogin.php"><button type="submit" class="oFood">ORDER</button></a>
+            </div>             
+        </div>   -->
+    </body>
+</html>
