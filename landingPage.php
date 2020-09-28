455ca57aea5e36bdbc5a2ef9d715d99676f4aa2f
StacyChebet <staceychebet23@gmail.com>
2020-09-28 14:50:53 +0300

    Added navbar with options to change password or logout

---
+++ landingPage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>HOME</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/landingPage.css">
+        <link href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Monoton&display=swap" rel="stylesheet">        
+
+        <script src="js/index.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="container">
+            <div id="topNavBar">
+                <img class="logo" src="images/camLogo.png">
+                <ul>
+                    <li id="title"><a href="landingPage.php">STACEGRAM</a></li>
+                    <li class="listItem"><a href="">Logout</a></li>
+                    <li class="listItem"><a href="">Change Password</a></li>
+                    </ul>
+            </div>
+        </div> 
+    </body>
+</html>
